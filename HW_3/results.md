62e2d30630f7fce159e336a8304bf6f9b76baa75
Kashyap <k.patel1098@gmail.com>
2021-03-30 21:56:33 -0500

    final code changes for hw_3

--- HW_3/results.md
+++ HW_3/results.md
@@ -1,44 +1,64 @@
+## Custom (My) CountVectorizer
                     precision    recall  f1-score   support
 
        hate_speech       0.41      0.32      0.36       454
-offensive_language       0.92      0.94      0.93      5757
+    offensive_language       0.92      0.94      0.93      5757
            neither       0.82      0.81      0.81      1224
 
           accuracy                           0.88      7435
          macro avg       0.71      0.69      0.70      7435
       weighted avg       0.87      0.88      0.87      7435
 
-[[ 144  267   43]
- [ 187 5389  181]
- [  23  206  995]]
-
                     precision    recall  f1-score   support
 
        hate_speech       0.37      0.33      0.35       421
-offensive_language       0.92      0.93      0.93      5751
+    offensive_language       0.92      0.93      0.93      5751
            neither       0.82      0.82      0.82      1263
 
           accuracy                           0.88      7435
          macro avg       0.70      0.69      0.70      7435
       weighted avg       0.87      0.88      0.88      7435
 
-[[ 139  238   44]
- [ 214 5353  184]
- [  26  203 1034]]
-
                     precision    recall  f1-score   support
 
        hate_speech       0.40      0.31      0.35       421
-offensive_language       0.92      0.94      0.93      5751
+    offensive_language       0.92      0.94      0.93      5751
            neither       0.83      0.81      0.82      1263
 
           accuracy                           0.88      7435
          macro avg       0.72      0.69      0.70      7435
       weighted avg       0.88      0.88      0.88      7435
 
-[[ 132  256   33]
- [ 168 5410  173]
- [  29  209 1025]]
+<hr>
 
+## Scikit Learn CountVectorizer
 
+                    precision    recall  f1-score   support
 
+       hate_speech       0.44      0.30      0.36       454
+    offensive_language       0.93      0.95      0.94      5732
+           neither       0.85      0.85      0.85      1249
+
+          accuracy                           0.89      7435
+         macro avg       0.74      0.70      0.71      7435
+      weighted avg       0.88      0.89      0.89      7435
+
+                    precision    recall  f1-score   support
+
+       hate_speech       0.44      0.33      0.38       436
+    offensive_language       0.93      0.94      0.93      5766
+           neither       0.82      0.84      0.83      1233
+
+          accuracy                           0.89      7435
+         macro avg       0.73      0.71      0.72      7435
+      weighted avg       0.88      0.89      0.88      7435
+
+                    precision    recall  f1-score   support
+
+       hate_speech       0.41      0.32      0.36       454
+    offensive_language       0.93      0.94      0.93      5700
+           neither       0.83      0.87      0.85      1281
+
+          accuracy                           0.89      7435
+         macro avg       0.73      0.71      0.72      7435
+      weighted avg       0.88      0.89      0.88      7435
--- HW_3/toxic_comment_classification.py
+++ HW_3/toxic_comment_classification.py
@@ -1,51 +1,99 @@
+from typing import Tuple, List
+
 import pandas as pd
 from sklearn import svm, metrics
 
 import gen_utils
 import result_utils
-from HW_3.vectorizer import CountVectorizer
 
 
-train_data = pd.read_csv(gen_utils.HATE_SPEECH_PATH, header=0)
-# class = 0 - hate speech 1 - offensive language 2 - neither
-labels = list(train_data["class"].array)
-text = list(train_data["tweet"].array)
+DEBUG_MODE = False
 
-# transform text into vectors
-vectorizer = CountVectorizer(text, labels, debug_mode=True)
-vectors = vectorizer.get_vectors()
 
-# split vector and labels into train/test sets
-input_array = list(zip(vectors, labels))
-train_data, test_data = gen_utils.split_data(input_array)
-train_text, train_labels = zip(*train_data)
-test_text, test_labels = zip(*test_data)
+def get_hate_speech_data() -> Tuple[List[str], List[int]]:
+    train_data = pd.read_csv(gen_utils.HATE_SPEECH_PATH, header=0)
+    # class = 0 - hate speech 1 - offensive language 2 - neither
+    labels = list(train_data["class"].array)
+    text = list(train_data["tweet"].array)
 
-# train SVM
-print("Training SVM")
-clf = svm.LinearSVC()
-clf.fit(train_text, train_labels)
+    return text, labels
 
-# predict and capture results
-results = result_utils.Results()
-all_predictions = []
-test_length = len(test_text)
-for i in range(test_length):
-    # get data to test
-    data, label = test_text[i], test_labels[i]
-    results.add_to_total()
-    print(f"Processing result: {i} / {test_length}")
 
-    # get prediction
-    predictions = clf.predict([data])
-    prediction = predictions[0]
-    all_predictions.append((data, predictions))
+def use_my_count_vectorizer(text: List[str], labels: List[int]) -> List[List[int]]:
+    # transform text into vectors (using my CountVectorizer)
+    from HW_3.vectorizer import CountVectorizer
+    vectorizer = CountVectorizer(text, labels, debug_mode=DEBUG_MODE)
+    vectors = vectorizer.get_vectors()
 
-    # add prediction to results
-    results.add_multiclass_result(result=prediction, expected_result=label)
+    return vectors
 
-results.report()
 
-data, predicted = zip(*all_predictions)
-print(metrics.classification_report(test_labels, predicted, target_names=["hate_speech", "offensive_language", "neither"]))
-print(metrics.confusion_matrix(test_labels, predicted))
+def use_sklearn_count_vectorizer(text: List[str]) -> List[List[int]]:
+    # using sklearn CountVectorizer
+    from sklearn.feature_extraction.text import CountVectorizer
+    vectorizer = CountVectorizer()
+    vector_holder = vectorizer.fit_transform(text)
+    vectors = vector_holder.toarray()
+
+    return vectors
+
+
+def transform_vectors_into_train_and_test(vectors: List[List[int]],
+                                          labels: List[int]) -> Tuple[List[Tuple[str, int]], List[Tuple[str, int]]]:
+    # split vector and labels into train/test sets
+    input_array = list(zip(vectors, labels))
+    train_data, test_data = gen_utils.split_data(input_array)
+
+    return train_data, test_data
+
+
+def predict_svm_and_export_results(clf, test_text_data: List[str], test_labels_data: List[int]):
+    # predict and capture results
+    results = result_utils.Results()
+    all_predictions = []
+    test_length = len(test_text_data)
+    for i in range(test_length):
+        # get data to test
+        data, label = test_text_data[i], test_labels_data[i]
+        results.add_to_total()
+
+        if DEBUG_MODE:
+            print(f"Processing result: {i} / {test_length}")
+
+        # get prediction
+        predictions = clf.predict([data])
+        prediction = predictions[0]
+        all_predictions.append((data, predictions))
+
+        # add prediction to results
+        results.add_multiclass_result(result=prediction, expected_result=label)
+
+    # output results
+    results.report()
+    data, predicted = zip(*all_predictions)
+    print(metrics.classification_report(test_labels_data, predicted,
+                                        target_names=["hate_speech", "offensive_language", "neither"]))
+    print(metrics.confusion_matrix(test_labels_data, predicted))
+
+
+if __name__ == '__main__':
+    # get input data
+    hate_text, hate_labels = get_hate_speech_data()
+
+    # use one vectorizer
+    hate_vectors = use_my_count_vectorizer(hate_text, hate_labels)
+    # hate_vectors = use_sklearn_count_vectorizer(hate_text)
+
+    # get final 70/30 split of data
+    v_train_data, v_test_data = transform_vectors_into_train_and_test(hate_vectors, hate_labels)
+    train_text, train_labels = zip(*v_train_data)
+    test_text, test_labels = zip(*v_test_data)
+
+    # train SVM classifier
+    if DEBUG_MODE:
+        print("Training SVM")
+    clf_classifier = svm.LinearSVC()
+    clf_classifier.fit(train_text, train_labels)
+
+    # predict test data and output results
+    predict_svm_and_export_results(clf_classifier, test_text, test_labels)
